4b0a8f702e8b98d066db55a6c4cfbcf5c67513dc
funai-yoshihisa-tanaka <yoshihisa-tanaka@funaisoken.co.jp>
2025-11-27 15:44:15 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+type Type1 = {
+  id: number;
+  text: string;
+}
+
+type Type2 = {
+  text: string;
+}
+
+
+const data1: Type1 = {
+  id: 1,
+  text: 'Text',
+}
+
+const data2 = data1 as Type2;
+
+console.log(data2); // 「{ id: 1, text: 'Text' }」が出力される
